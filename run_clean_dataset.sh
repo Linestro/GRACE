fd703abe25f68bdc1b2fd9398e15e20d71b3bf7a
yehaojie <yehaojie@umich.edu>
2023-01-27 20:32:57 -0500

    clean dataset reverse changes

--- run_clean_dataset.sh
+++ run_clean_dataset.sh
@@ -15,33 +15,17 @@ fi
 # fi
 
 if [[ $workload == "anime" ]]; then
+  data_path="./dataset/anime/rating_preprocess.csv"
   post_data_path="./dataset/anime/rating_postprocess.csv"
-elif [[ $workload == "office" ]]; then
-  post_data_path="./dataset/office/amazon_Office_Products_postprocess.csv"
-elif [[ $workload == "dblp" ]]; then
-  post_data_path="./dataset/dblp/dblp_postprocess.csv"
-elif [[ $workload == "sports" ]]; then
-  post_data_path="./dataset/sports/amazon_Sports_and_Outdoors_postprocess.csv"
-elif [[ $workload == "M1" ]]; then
-  post_data_path="./dataset/merged/merged_twitch_movie_anime_steam.csv"
-  aux_data_path="./dataset/merged/merged_twitch_movie_anime_steam.aux"
-elif [[ $workload == "M2" ]]; then
-  post_data_path="./dataset/merged/merged_clothes_office_dblp_anime.csv"
-  aux_data_path="./dataset/merged/merged_clothes_office_dblp_anime.aux"
-elif [[ $workload == "M3" ]]; then
-  post_data_path="./dataset/merged/merged_sports_office_dblp_twitch.csv"
-  aux_data_path="./dataset/merged/merged_sports_office_dblp_twitch.aux"
-elif [[ $workload == "M4" ]]; then
-  post_data_path="./dataset/merged/merged_sports_clothes_office_dblp.csv"
-  aux_data_path="./dataset/merged/merged_sports_clothes_office_dblp.aux"
 elif [[ $workload == "steam" ]]; then
+  data_path="./dataset/steam/australian_users_items_preprocess.csv"
   post_data_path="./dataset/steam/australian_users_items_postprocess.csv"
 elif [[ $workload == "movie" ]]; then
+  data_path="./dataset/movie/rating_preprocess.csv"
   post_data_path="./dataset/movie/rating_postprocess.csv"
 elif [[ $workload == "twitch" ]]; then
+  data_path="./dataset/twitch/100k_a_preprocess.csv"
   post_data_path="./dataset/twitch/100k_a_postprocess.csv"
-elif [[ $workload == "clothes" ]]; then
-  post_data_path="./dataset/clothes/amazon_Clothing_Shoes_and_Jewelry_postprocess.csv"
 else
     echo "invalid workload given in argv 1"
     exit 0
