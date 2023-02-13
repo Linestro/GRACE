6cd1e35693a5506edc09ffcd474edc6da8af0d73
yehaojie <yehaojie@umich.edu>
2023-02-12 19:21:08 -0500

    remove & in generating ICG

--- run_build_graph_group.sh
+++ run_build_graph_group.sh
@@ -11,7 +11,7 @@ workloads=("anime" "steam" "movie" "sports" "twitch" "clothes" "dblp" "office")
 # workloads=()
 for workload in "${workloads[@]}"
 do
-./run_build_graph.sh ${workload} &
+./run_build_graph.sh ${workload} 
 done
 
 wait
--- run_build_graph_merged_group.sh
+++ run_build_graph_merged_group.sh
@@ -12,7 +12,7 @@ workloads=("M1" "M2" "M3" "M4")
 
 for workload in "${workloads[@]}"
 do
-./run_build_graph_merged.sh ${workload} &
+./run_build_graph_merged.sh ${workload} 
 done
 
 wait
